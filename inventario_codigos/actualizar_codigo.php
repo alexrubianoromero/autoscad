029e32799b75e1a03946e14cd38a6aa23b3c2b88
Alex Rubiano <alexerr@algartech.com>
2025-05-21 16:35:09 -0500

    mejora de adicion de existencias

--- inventario_codigos/actualizar_codigo.php
+++ inventario_codigos/actualizar_codigo.php
@@ -1,33 +1,49 @@
 <?php
 session_start();
-/*
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
-*/
+
+// echo '<pre>';
+// print_r($_REQUEST);
+// echo '</pre>';
+// die();
 //exit();
 // tipo movimiento 1 es saldo inicial 
 // tipo movimiento 2 es entrada
 // tipo movimiento 3 es salida 
 include('../valotablapc.php');  
-$nueva_cantidad = $_POST['cantidad_actual'] + $_POST['cantidad'];
-
+$nueva_cantidad = $_REQUEST['cantidad_actual'] + $_REQUEST['cantidad'];
+$_SESSION['id_empresa'] = '94';
 //primero grabar el movimiento enb la tabla de movimientos 
-$sql_grabar_movimiento = "insert into $tabla19 (fecha_movimiento,cantidad,observaciones,tipo_movimiento,facturacompra,id_codigo_producto,id_empresa) 
-values ('".$_POST['fecha']."','".$_POST['cantidad']."','".$_POST['observaciones']."','2','".$_POST['facturacompra']."',
-'".$_POST['id_codigo']."','".$_SESSION['id_empresa']."'
+$sql_grabar_movimiento = "insert into $tabla19 
+(fecha_movimiento,cantidad,observaciones,tipo_movimiento,facturacompra,id_codigo_producto,id_empresa,costoUnitario) 
+values (
+          '".$_REQUEST['fecha']."'
+          ,'".$_REQUEST['cantidad']."'
+          ,'".$_REQUEST['observaciones']."'
+          ,'2'
+          ,'".$_REQUEST['facturacompra']."'
+          ,'".$_REQUEST['id_codigo']."'
+          ,'".$_SESSION['id_empresa']."'
+          ,'".$_REQUEST['costoUnitario']."'
 )";
 //echo '<br>consulta '. $sql_grabar_movimiento;
 //exit();
 $grabar_movimiento = mysql_query($sql_grabar_movimiento,$conexion);
 
 //segundo actualizar las existencias del iventario 
-$sql_actualizar_codigo = "update $tabla12 set cantidad =  '".$nueva_cantidad ."'
-   where    id_codigo = '".$_POST['id_codigo']."'    and id_empresa =  '".$_SESSION['id_empresa']."'     " ;
- //echo '<br>consulta '. $sql_actualizar_codigo;
+$sql_actualizar_codigo = 
+"update $tabla12 
+set cantidad =  '".$nueva_cantidad ."'
+    ,valor_unit =  '".$_REQUEST['costoUnitario']."'   
+  where    id_codigo = '".$_REQUEST['id_codigo']."'    
+  and id_empresa =  '".$_SESSION['id_empresa']."'  " ;
+//  die($sql_actualizar_codigo);
 $consulta_grabar_codigo = mysql_query($sql_actualizar_codigo,$conexion);
 
-echo '<br>CODIGO ACTUALIZADO';
+echo '<div style="font-size:25px;">';
+echo 'CODIGO ACTUALIZADO';
+echo '<br>Ahora el costo unitario es :'.$_REQUEST['costoUnitario'];
+echo '<br>Ahora la cantidad es  : '.$nueva_cantidad;
+echo '</div>';
 //include('../colocar_links2.php');
 //echo '<br><a href="index.php">Menu Anterior</a>';
 
--- inventario_codigos/adicion_existencias_codigo.php
+++ inventario_codigos/adicion_existencias_codigo.php
@@ -18,55 +18,63 @@ $fechapan =  time();
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" href="../css/normalize.css">
-	<link rel="stylesheet" href="../css/style.css">
+	<!-- <link rel="stylesheet" href="../css/normalize.css">
+	<link rel="stylesheet" href="../css/style.css"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </head>
 <body>
 <? include("../empresa.php"); ?>
-<Div id="contenidos">
-		<header>
-			<h1><? echo $empresa; ?> </h1>
-			<h2><? echo $slogan; ?>  <h2>
-		</header>
-	<section>
-	<article>
+<Div id="contenidos" class="container mt-3">
+	
+
       
-	  <table width="767" border="1">
+	  <!-- <table width="767" border="1" > -->
+	<table class="table" >
   <tr>
-    <td width="107"><h3>CODIGO</h3></td>
-    <td width="247"><h3>DESCRIPCION</h3></td>
-    <td width="181"><h3>VALOR _UNIT</h3> </td>
-    <td width="204"><h3>CANTIDAD ACTUAL </h3></td>
+    <th >CODIGO</th>
+    <th >DESCRIPCION</th>
+    <th >VALOR _UNIT </th>
+    <th >CANTIDAD ACTUAL</th>
   </tr>
   <tr>
-    <td><h3><?php echo $datos_codigo['codigo_producto'] ?></h3></td>
-    <td><h3><?php echo $datos_codigo['descripcion'] ?></h3></td>
-    <td><h3><?php echo $datos_codigo['valor_unit'] ?></h3></td>
-    <td><h3><?php echo $datos_codigo['cantidad'] ?></h3></td>
+    <td><?php echo $datos_codigo['codigo_producto'] ?></td>
+    <td><?php echo $datos_codigo['descripcion'] ?></td>
+    <td><?php echo $datos_codigo['valor_unit'] ?></td>
+    <td><?php echo $datos_codigo['cantidad'] ?></td>
   </tr>
 </table>
 <br>
 
-<div id = "datos">
+<div id = "div_datos" class="container row">
+  <!-- <div class="col-lg-3">
+     <label>Fecha:</label>
+     <input size=10 name=fecha id = "fecha"  value= <? echo date ( "Y/m/j" , $fechapan );?>>
+  </div> -->
 <table width="603" border="1">
   <tr>
     <td width="179">FECHA</td>
-    <td width="408"><input size=10 name=fecha id = "fecha"  value= <? echo date ( "Y/m/j" , $fechapan );?>></td>
+    <td width="408"><input class="form-control" size=10 name=fecha id = "fecha"  value= <? echo date ( "Y/m/j" , $fechapan );?>></td>
   </tr>
   <tr>
     <td>FACTURA COMPRA </td>
     <td><label>
-      <input type="text" name="facturacompra"  id = "facturacompra"  >
+      <input class="form-control" type="text" name="facturacompra"  id = "facturacompra"  >
     </label></td>
   </tr>
   <tr>
     <td>CANTIDAD A CARGAR </td>
-    <td><input type="text" name="cantidad"  id = "cantidad"  ></td>
+    <td><input class="form-control" type="text" name="cantidad"  id = "cantidad"  ></td>
   </tr>
+  <tr>
+    <td>COSTO UNITARIO </td>
+    <td><input class="form-control" type="text" name="costoUnitario"  id = "costoUnitario"  ></td>
+  </tr>
+
   <tr>
     <td>OBSERVACIONES </td>
     <td><label>
-      <textarea name="observaciones" id = "observaciones" cols="60" rows="4"></textarea>
+      <textarea class="form-control" name="observaciones" id = "observaciones" cols="60" rows="4"></textarea>
     </label></td>
   </tr>
   <tr>
@@ -75,56 +83,116 @@ $fechapan =  time();
 	<input type="hidden" name="cantidad_actual"  id = "cantidad_actual" value = "<?php   echo $datos_codigo['cantidad']  ?>  "  ></td>
   </tr>
   <tr>
-    <td colspan="2"><label><button type ="button"  id = "actualizar_codigo" >
-      <h3>ACTUALIZAR_INVENTARIO_CODIGO</h3>
-    </button></label></td>
+    <!-- <td colspan="2"><label><button type ="button"  id = "actualizar_codigo" onclick="actualizarCodigoNuevo();" > -->
+    <td colspan="2" align="center">
+      <button class="btn btn-primary " style="margin:5px;" type ="button"   onclick="actualizarCodigoNuevo('<?php   echo  $_REQUEST['id_codigo'] ?>');" >
+      ACTUALIZAR_INVENTARIO_CODIGO
+    </button></td>
     </tr>
 </table>
 </div>
-<BR>
-
-<?php 
-include('../colocar_links2.php');
-?>
-	<p>&nbsp;</p>
-	</article>
-	</section>
-	
 </Div>
 	
 </body>
 </html>
-<script src="../js/modernizr.js"></script>   
-<script src="../js/prefixfree.min.js"></script>
-<script src="../js/jquery-2.1.1.js"></script> 
+<!-- <script src="../js/modernizr.js"></script>   
+<script src="../js/prefixfree.min.js"></script> -->
+<!-- <script src="../js/jquery-2.1.1.js"></script>  -->
 
-<script language="JavaScript" type="text/JavaScript">
+<script>
+
+    function actualizarCodigoNuevo(id_codigo)
+    {
+      event.preventDefault();
+      var validacion = validacionCampos();
+      if(validacion)
+      {
+        // alert(idCodigo);
+                var fecha = document.getElementById('fecha').value;
+                var facturacompra = document.getElementById('facturacompra').value;
+                var cantidad = document.getElementById('cantidad').value;
+                var observaciones = document.getElementById('observaciones').value;
+                var costoUnitario = document.getElementById('costoUnitario').value;
+                var cantidad_actual = document.getElementById('cantidad_actual').value;
+                const http=new XMLHttpRequest();
+                const url = 'actualizar_codigo.php';
+                http.onreadystatechange = function(){
+                    if(this.readyState == 4 && this.status ==200){
+                        document.getElementById("div_datos").innerHTML = this.responseText;
+                    }
+                };
+                http.open("POST",url);
+                http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                http.send("opcion=1"
+                + "&id_codigo="+id_codigo
+                + "&fecha="+fecha
+                + "&facturacompra="+facturacompra
+                + "&cantidad="+cantidad
+                + "&observaciones="+observaciones
+                + "&costoUnitario="+costoUnitario
+                + "&cantidad_actual="+cantidad_actual
+                );
+      }
+      
+    }
+
+    function validacionCampos()
+    {
+       if(document.getElementById("facturacompra").value == '')
+        {
+          alert("Digite facturacompra") ;  
+          document.getElementById("facturacompra").focus();
+          return 0;
+        }
+
+       if(document.getElementById("cantidad").value == '')
+        {
+          alert("Digite cantidad") ;  
+          document.getElementById("cantidad").focus();
+          return 0;
+        }
+       if(document.getElementById("costoUnitario").value == '')
+        {
+          alert("Digite costoUnitario") ;  
+          document.getElementById("costoUnitario").focus();
+          return 0;
+        }
+       if(document.getElementById("observaciones").value == '')
+        {
+          alert("Digite observaciones") ;  
+          document.getElementById("observaciones").focus();
+          return 0;
+        }
+
+
+        return 1
+    }
+
             
-			$(document).ready(function(){
-               
-					
-					/////////////////////////
-					$("#actualizar_codigo").click(function(){
-							var data =  'id_codigo=' + $("#id_codigo").val();
-							data += '&fecha=' + $("#fecha").val();
-							data += '&facturacompra=' + $("#facturacompra").val();
-							data += '&cantidad=' + $("#cantidad").val();
-							data += '&observaciones=' + $("#observaciones").val();
-							data += '&cantidad_actual=' + $("#cantidad_actual").val();
-							$.post('actualizar_codigo.php',data,function(a){
-							//$(window).attr('location', '../index.php);
-							$("#datos").html(a);
-								//alert(data);
-							});	
-						 });
-					////////////////////////
-					
-			
-			});		////finde la funcion principal de script
-			
-			
-			
-			
+			// $(document).ready(function(){
+
+        // $("#actualizar_codigo").click(function(){
+        //     var valida = validarInfo()
+        //     if(valida);
+        //     {
+        //       var data =  'id_codigo=' + $("#id_codigo").val();
+				// 			data += '&fecha=' + $("#fecha").val();
+				// 			data += '&facturacompra=' + $("#facturacompra").val();
+				// 			data += '&cantidad=' + $("#cantidad").val();
+				// 			data += '&observaciones=' + $("#observaciones").val();
+				// 			data += '&costoUnitario=' + $("#costoUnitario").val();
+				// 			data += '&cantidad_actual=' + $("#cantidad_actual").val();
+				// 			$.post('actualizar_codigo.php',data,function(a){
+        //         //$(window).attr('location', '../index.php);
+        //         $("#datos").html(a);
+				// 			});	
+        //     }
+				// 	});
+				// 	////////////////////////
+        // });		////finde la funcion principal de script
+        
+           
+        
 			
 </script>
 
